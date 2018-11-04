a7d8e0117e0af6dcb8ccc91394bd2600fee7ef9a
AdaSmuk <40755968+AdaSmuk@users.noreply.github.com>
2018-11-04 14:21:47 +0100

    [NEW] Main Hub Buttons!
    Just for the looks

--- src/app/MainHub/MainHubButtonsControler/main-hub-buttons.component.ts
+++ src/app/MainHub/MainHubButtonsControler/main-hub-buttons.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 
+export enum ToogleEnum {
+  ON = 'ON',
+  OFF = 'OFF'
+}
+
 @Component({
   selector: 'main-hub-buttons',
   templateUrl: './main-hub-buttons.view.html'
@@ -8,8 +13,23 @@ export class MainHubButtonsComponent implements OnInit {
 
   @HostBinding('class') styleClass = 'main-hub-buttons';
 
+  public toggleLightButtonCaption: ToogleEnum;
+
   constructor() {}
 
+  public toggleLightButton(): void {
+    this.toggleLightButtonCaption = this.toggleLightButtonCaption === ToogleEnum.ON ? ToogleEnum.OFF : ToogleEnum.ON;
+  }
+
+  public getCurrentSensorData(): void {
+    console.log('Cannot get data. Method not implemented');
+  }
+
+  public printPDF(): void {
+    console.log('Cannot print PDF. Method not implemented');
+  }
+
   ngOnInit() {
+    this.toggleLightButtonCaption = ToogleEnum.OFF;
   }
 }
