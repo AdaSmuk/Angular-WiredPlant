a724a08c924fd149718fd81dea203efa61b1e5e4
AdaSmuk <40755968+AdaSmuk@users.noreply.github.com>
2018-11-04 16:57:13 +0100

    [NEW] Graph Main Hub
    Damn, it even looks quite good.

--- src/app/MainHub/MainHubGraphComponent/main-hub-graph.component.ts
+++ src/app/MainHub/MainHubGraphComponent/main-hub-graph.component.ts
@@ -8,8 +8,123 @@ export class MainHubGraphComponent implements OnInit {
 
   @HostBinding('class') styleClass = 'main-hub-graph';
 
-  constructor() {}
+  public soilMoistureDataSource: Object;
+  public temperatureDataSource: Object;
+  public sunlightDataSource: Object;
+  public soilMoistureWidth: number;
+  public soilMoistureHeight: number;
+  public sunlightHeight: number;
+  public sunlightWidth: number;
+  public temperatureHeight: number;
+  public temperatureWidth: number;
+
+  private currentTemperaturColor: string;
+
+  constructor() {
+    this.currentTemperaturColor = '#7f8942';
+    this.temperatureDataSource = {
+      'chart': {
+        'caption': 'Temperature Monitor',
+        'lowerLimit': '-30',
+        'upperLimit': '50',
+        'decimals': '1',
+        'numberSuffix': '°C',
+        'showhovereffect': '1',
+        'thmFillColor': this.currentTemperaturColor,
+        'showGaugeBorder': '1',
+        'gaugeBorderColor': '#008ee4',
+        'gaugeBorderThickness': '2',
+        'gaugeBorderAlpha': '30',
+        'thmOriginX': '100',
+        'chartBottomMargin': '20',
+        'valueFontColor': '#000000',
+        'theme': 'fusion'
+      },
+      'value': '8',
+      'annotations': {
+        'showbelow': '1',
+      }
+    };
+    this.sunlightDataSource = {
+      'chart': {
+        'caption': 'Light level status',
+        'upperlimit': '65535',
+        'lowerlimit': '0',
+        'captionPadding': '30',
+        'showshadow': '0',
+        'showvalue': '1',
+        'useColorNameAsValue': '1',
+        'placeValuesInside': '1',
+        'valueFontSize': '16',
+        'plottooltext': '<span id=\'headerdiv\' style=\'font-family:"Arial";font-size: 13px;font-weight: bold;\'>Current light intensivity:</span>{br}<div id=\'valueDiv\' style=\' color: #ffd756; text-align:center;font-size: 25px; padding: 10px;  margin-top:5px; font-family:"Arial"; font-weight: bold;\'>$value lx</div>',
+        'theme': 'fusion'
+      },
+      'colorrange': {
+        'color': [
+          {
+            'minvalue': '0',
+            'maxvalue': '5000',
+            'label': 'It\'s too dark <br> (unless it\'s night-time)',
+            'code': '#cccccc'
+          },
+          {
+            'minvalue': '5000',
+            'maxvalue': '30000',
+            'label': 'A bit cloudy',
+            'code': '#82baa9'
+          },
+          {
+            'minvalue': '30000',
+            'maxvalue': '65535',
+            'label': 'Bright and sunny!',
+            'code': '#ffd756'
+          }
+        ]
+      },
+      'value': '29000'
+    };
+    this.soilMoistureDataSource = {
+      'chart': {
+        'caption': 'Current soil moisture',
+        'renderAt': 'chartContainer',
+        'lowerLimit': '0',
+        'upperLimit': '100',
+        'showValue': '1',
+        'numberSuffix': '',
+        'theme': 'fusion',
+        'showToolTip': '0'
+      },
+      'colorRange': {
+        'color': [{
+          'minValue': '0',
+          'maxValue': '45',
+          'code': '#7f8942'
+        }, {
+          'minValue': '45',
+          'maxValue': '70',
+          'code': '#ffd756'
+        }, {
+          'minValue': '70',
+          'maxValue': '100',
+          'code': '#cc4b37'
+        }]
+      },
+      'dials': {
+        'dial': [{
+          'value': '81'
+        }]
+      }
+    };
+  }
 
   ngOnInit() {
+    this.soilMoistureHeight = 200;
+    this.soilMoistureWidth = 320;
+
+    this.sunlightHeight = 250;
+    this.sunlightWidth = 250;
+
+    this.temperatureHeight = 250;
+    this.temperatureWidth = 200;
   }
 }
--- src/app/MainHub/main-hub.module.ts
+++ src/app/MainHub/main-hub.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { MainHubComponent } from './main-hub.component';
 import { MainHubAlertComponent } from './MainHubAlertComponent/main-hub-alert.component';
 import { MainHubGraphComponent } from './MainHubGraphComponent/main-hub-graph.component';
 import { MainHubButtonsComponent } from './MainHubButtonsControler/main-hub-buttons.component';
+
+import { FusionChartsModule } from 'angular-fusioncharts';
+import * as FusionCharts from 'fusioncharts';
+import * as Widgets from 'fusioncharts/fusioncharts.widgets';
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+FusionChartsModule.fcRoot(FusionCharts, Widgets, FusionTheme);
 
 @NgModule({
     declarations: [
         MainHubComponent, MainHubAlertComponent, MainHubGraphComponent, MainHubButtonsComponent
     ],
     imports: [
-        CommonModule
+        CommonModule, FormsModule, HttpModule, FusionChartsModule
     ],
     providers: [],
     exports: [MainHubComponent, MainHubAlertComponent, MainHubGraphComponent, MainHubButtonsComponent]
